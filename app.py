b4e2b3ea8576fc64347ba89b6d7203dc491da7c1
Yaseen Shaik <smdyaseen15@gmail.com>
2025-12-09 18:57:03 +0000

    added deploy workflow

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
+
+# LOG TO BOTH FILE AND CONSOLE
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+app.logger.addHandler(file_handler)
 
 @app.route("/")
 def home():
-    return {"message": "Backend is working on Ubuntu!"}
+    app.logger.info("Home API called")
+    return {"message": "Backend is working on Ubuntu & EC2!"}
+
+@app.route("/test")
+def test():
+    app.logger.info("Test API called")
+    return {"message": "Log testing successful"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
